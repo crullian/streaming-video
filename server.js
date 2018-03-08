02e9dc75e07d65126ad792c4b10f5a7c853057a3
crullian <crullian@gmail.com>
2018-03-07 23:57:37 -0500

    Cleanup, semis, destructure fs, remove console logs.

--- server.js
+++ server.js
@@ -1,50 +1,47 @@
-const express = require('express')
-const fs = require('fs')
-const path = require('path')
-const app = express()
+const express = require('express');
+const { createReadStream, statSync } = require('fs');
+const path = require('path');
+const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'))
-})
+});
 
 app.get('/video', function(req, res) {
-  const path = 'assets/sample.mp4'
-  const stat = fs.statSync(path)
-  const fileSize = stat.size
-  const range = req.headers.range
+  const path = 'assets/shetland_04_01.mp4';
+  const stat = statSync(path);
+  const fileSize = stat.size;
+  const range = req.headers.range;
 
   if (range) {
-    console.log('RANGE', range);
-    const parts = range.replace(/bytes=/, "").split("-")
-    const start = parseInt(parts[0], 10)
+    const parts = range.replace(/bytes=/, "").split("-");
+    const start = parseInt(parts[0], 10);
     const end = parts[1]
       ? parseInt(parts[1], 10)
       : fileSize-1
 
-    const chunksize = (end-start)+1
-    const file = fs.createReadStream(path, {start: start, end: end})
+    const chunksize = (end-start)+1;
     const head = {
       'Content-Range': `bytes ${start}-${end}/${fileSize}`,
       'Accept-Ranges': 'bytes',
       'Content-Length': chunksize,
       'Content-Type': 'video/mp4'
-    }
+    };
 
-    res.writeHead(206, head)
-    file.pipe(res)
+    res.writeHead(206, head);
+    createReadStream(path, {start: start, end: end}).pipe(res);
   } else {
-    console.log('IN ELSE')
     const head = {
       'Content-Length': fileSize,
       'Content-Type': 'video/mp4'
-    }
-    res.writeHead(200, head)
-    fs.createReadStream(path).pipe(res)
+    };
+    res.writeHead(200, head);
+    createReadStream(path).pipe(res);
   }
-})
+});
 
 app.listen(3000, function () {
   console.log('Fun happens on port 3000')
-})
+});
